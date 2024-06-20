637a0550dcafe302597de68c519fde8b8d45f086
Visotsky Maxim <needlife124@gmail.com>
2024-06-20 10:15:27 +0300

    test

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import viteImagemin from 'vite-plugin-imagemin';
+import postcssConfig from './postcss.config.ts';
+
+export default defineConfig({
+  plugins: [
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7, interlaced: false },
+      optipng: { optimizationLevel: 7 },
+      pngquant: { quality: [0.65, 0.8], speed: 4 },
+      mozjpeg: { quality: 75 },
+      svgo: {
+        plugins: [{ removeViewBox: false }, { removeEmptyAttrs: false }],
+      },
+      webp: { quality: 75 },
+    }),
+  ],
+  css: {
+    postcss: postcssConfig,
+  },
+});
