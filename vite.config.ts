b6ef71ef21aa84578321f21a548b714aa5cd78e6
Visotsky Maxim <needlife124@gmail.com>
2024-07-02 10:28:20 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 // import { defineConfig } from 'vite';
-// import { fileURLToPath } from 'url';
 // import viteImagemin from 'vite-plugin-imagemin';
 // import postcssConfig from './postcss.config.ts';
 // import injectHTML from 'vite-plugin-html-inject';
 // import FullReload from 'vite-plugin-full-reload';
-// import glob from 'fast-glob';
-
-// const urlToPath = (url) => fileURLToPath(new URL(url));
+// import fg from 'fast-glob';
 
 // export default defineConfig({
 //   base: './',
@@ -30,15 +27,16 @@
 //   build: {
 //     minify: false,
 //     rollupOptions: {
-//       input: Object.fromEntries(
-//         glob
-//           .sync(['./*.html', './pages/**/*.html'])
-//           .map((file) => [
-//             file.slice(0, file.lastIndexOf('.')),
-//             urlToPath(new URL(file, import.meta.url)),
-//           ]),
-//       ),
-
+//       input: fg
+//         .sync(['./*.html', './src/**/*.html'])
+//         .reduce((entries, file) => {
+//           const name = file.slice(
+//             file.lastIndexOf('/') + 1,
+//             file.lastIndexOf('.'),
+//           );
+//           entries[name] = file;
+//           return entries;
+//         }, {}),
 //       output: {
 //         assetFileNames: 'assets/[name].[ext]',
 //       },
@@ -47,48 +45,54 @@
 // });
 
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import viteImagemin from 'vite-plugin-imagemin';
 import postcssConfig from './postcss.config.ts';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import glob from 'fast-glob';
+import fg from 'fast-glob';
 
-const urlToPath = (url) => fileURLToPath(new URL(url));
-
-export default defineConfig({
-  base: './',
-  plugins: [
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7, interlaced: false },
-      optipng: { optimizationLevel: 7 },
-      pngquant: { quality: [0.65, 0.8], speed: 4 },
-      mozjpeg: { quality: 75 },
-      svgo: {
-        plugins: [{ removeViewBox: false }, { removeEmptyAttrs: false }],
-      },
-      webp: { quality: 75 },
-    }),
-    injectHTML(),
-    FullReload(['./src/**/**.html', './src/**/*.css']),
-  ],
-  css: {
-    postcss: postcssConfig,
-  },
-  build: {
-    minify: false,
-    rollupOptions: {
-      input: Object.fromEntries(
-        glob
-          .sync(['./*.html', './pages/**/*.html'])
-          .map((file) => [
-            file.slice(0, file.lastIndexOf('.')),
-            urlToPath(new URL(file, import.meta.url)),
-          ]),
-      ),
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      ...{
+        [command === 'serve' ? 'global' : '_global']: {},
       },
     },
-  },
+    base: './',
+    plugins: [
+      viteImagemin({
+        gifsicle: { optimizationLevel: 7, interlaced: false },
+        optipng: { optimizationLevel: 7 },
+        pngquant: { quality: [0.65, 0.8], speed: 4 },
+        mozjpeg: { quality: 75 },
+        svgo: {
+          plugins: [{ removeViewBox: false }, { removeEmptyAttrs: false }],
+        },
+        webp: { quality: 75 },
+      }),
+      injectHTML(),
+      FullReload(['./src/**/**.html', './src/**/*.css']),
+    ],
+    css: {
+      postcss: postcssConfig,
+    },
+    build: {
+      minify: false,
+      rollupOptions: {
+        input: fg
+          .sync(['./*.html', './src/**/*.html'])
+          .reduce((entries, file) => {
+            const name = file.slice(
+              file.lastIndexOf('/') + 1,
+              file.lastIndexOf('.'),
+            );
+            entries[name] = file;
+            return entries;
+          }, {}),
+        output: {
+          assetFileNames: 'assets/[name].[ext]',
+        },
+      },
+    },
+  };
 });
