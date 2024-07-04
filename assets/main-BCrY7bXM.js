db5b449f8a8063921f210ec2c5dfb8b38caae9fd
Needlife1 <111612083+Needlife1@users.noreply.github.com>
2024-07-04 10:43:17 +0000

    Deploying to gh-pages from @ Needlife1/macarons--shop@47ff8fa830988b4b5d0a8bef6f3cfeb068eca275 🚀

--- assets/main-BCrY7bXM.js
+++ assets/main-BCrY7bXM.js
@@ -35,28 +35,71 @@
     fetch(link.href, fetchOpts);
   }
 })();
-const basketBtn = document.querySelector(".nav-basket-btn");
+const basketBtn = document.querySelectorAll(".nav-basket-btn");
 const backdrop = document.querySelector(".backdrop");
 const basket = document.querySelector(".basket");
 const basketCloseBtn = document.querySelector(".basket-close-btn");
 const basketFooterBtn = document.querySelectorAll(".basket-footer-btn");
-basketBtn == null ? void 0 : basketBtn.addEventListener("click", toggleVisibility);
-basketCloseBtn == null ? void 0 : basketCloseBtn.addEventListener("click", toggleVisibility);
-basketFooterBtn.forEach((el) => {
-  el == null ? void 0 : el.addEventListener("click", toggleVisibility);
+const positionContainer = document.querySelector(
+  ".position-container"
+);
+const body = document.querySelector("body");
+basketBtn.forEach((el) => {
+  console.log(el);
+  el == null ? void 0 : el.addEventListener("click", toggleVisibilityBasket);
 });
-function toggleVisibility() {
-  if (basket && backdrop) {
+basketCloseBtn == null ? void 0 : basketCloseBtn.addEventListener("click", toggleVisibilityBasket);
+basketFooterBtn.forEach((el) => {
+  el == null ? void 0 : el.addEventListener("click", toggleVisibilityBasket);
+});
+function toggleVisibilityBasket() {
+  if (basket && backdrop && positionContainer && body) {
     if (basket.classList.contains("closed")) {
       backdrop.style.display = "block";
+      body.style.overflow = "hidden";
+      positionContainer.style.pointerEvents = "all";
       basket.classList.remove("visually-hidden");
       setTimeout(() => {
         basket.classList.remove("closed");
       }, 10);
     } else {
+      body.style.overflow = "auto";
+      positionContainer.style.pointerEvents = "none";
       basket.classList.add("closed");
       setTimeout(() => {
         basket.classList.add("visually-hidden");
+      }, 500);
+      setTimeout(() => {
+        backdrop.style.display = "none";
+      }, 500);
+    }
+  }
+}
+const contacts = document.querySelector(".contacts");
+const closeContactsBtn = document.getElementById("contacts-close-btn");
+const openContactsBtn = document.querySelectorAll("#open-contacts");
+openContactsBtn.forEach((el) => {
+  el.addEventListener("click", toggleVisibilityContacts);
+});
+if (closeContactsBtn) {
+  closeContactsBtn.addEventListener("click", toggleVisibilityContacts);
+}
+function toggleVisibilityContacts() {
+  if (contacts && backdrop && positionContainer && body) {
+    if (contacts.classList.contains("closed")) {
+      backdrop.style.display = "block";
+      body.style.overflow = "hidden";
+      positionContainer.style.pointerEvents = "all";
+      contacts.classList.remove("visually-hidden");
+      setTimeout(() => {
+        contacts.classList.remove("closed");
+      }, 10);
+    } else {
+      body.style.overflow = "auto";
+      positionContainer.style.pointerEvents = "none";
+      contacts.classList.add("closed");
+      setTimeout(() => {
+        contacts.classList.add("visually-hidden");
       }, 500);
       setTimeout(() => {
         backdrop.style.display = "none";
